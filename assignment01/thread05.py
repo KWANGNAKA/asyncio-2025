0764c3ebd0d46c948c1b5fb39a2f8aea121aa1df
KWANGNAKA <6610301005@cdti.ac.th>
2025-06-18 11:12:42 +0700

    new5

--- assignment01/thread05.py
+++ assignment01/thread05.py
@@ -1,20 +1,22 @@
-# running a function with arguments in another thread
-from time import sleep, ctime
-from threading import Thread
+# Starting a Thread
+import threading
+import logging
+import time
 
-# a custom function that blocks for a moment
-def task(sleep_time, message):
-    # block for a moment
-    sleep(sleep_time)
-    # display a message
-    print(f'{ctime()} {message}')
+def thread_function(name):
+    logging.info("Thread %s: starting", name)
+    time.sleep(2)
+    logging.info("Thread %s: finishing", name)
 
-# create a thread
-thread = Thread(target=task, args=(1.5, 'New message from another thread'))
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, 
+                        datefmt="%H:%M:%S")
 
-# run the thread
-thread.start()
-
-# wait for the thread to finish
-print(f'{ctime()} Waiting for the thread...')
-thread.join()
+    logging.info("Main: before creating thread")
+    x = threading.Thread(target=thread_function, args=(1,))
+    logging.info("Main: before running thread")
+    x.start()
+    logging.info("Main: wait for the thread to finish")
+    # x.join()
+    logging.info("Main: all done")
