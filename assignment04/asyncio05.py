1ba3d983cda02ea24e76bfaa585b6493b233e4c2
KWANGNAKA <6610301005@cdti.ac.th>
2025-06-25 11:56:43 +0700

    ass4 br

--- assignment04/asyncio05.py
+++ assignment04/asyncio05.py
@@ -1,1 +1,13 @@
 # Check if a Task is Done
+import asyncio
+
+async def simmple_pask():
+    await asyncio.sleep(1)
+    return "เสร็จแล้ว!"
+async def main():
+    task = asyncio.create_task(simmple_pask())
+    print("ก่อน await:", task.done())
+    await task
+    print("หลัง await:", task.done()) #เสร็จแล้ว
+
+asyncio.run(main())
