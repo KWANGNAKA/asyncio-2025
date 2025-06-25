d592f26953be2b5a3274055082d44a188fd340b1
KWANGNAKA <6610301005@cdti.ac.th>
2025-06-25 11:03:26 +0700

    25.6.68

--- assignment03/asyncio04.py
+++ assignment03/asyncio04.py
@@ -1,1 +1,14 @@
-# Starting task
+#Event loop
+import asyncio
+
+async def green():
+    print("Hello")
+    await asyncio.sleep(1)
+    print("Word")
+
+asyncio.run(green()) #สร้างและ run event loop
+#
+# loop = asyncio.new_evnet_loop()
+# asyncio.set_event_loop(loop)
+# loop.run_until_complete(greet())
+# loop.close()
--- assignment04/asyncio01.py
+++ assignment04/asyncio01.py
@@ -1,1 +1,14 @@
 # example of getting the current task from the main coroutine
+import asyncio
+
+#define a mne coroutine
+async def main():
+    #report a message
+    print('main coroutine started')
+    #get the current task
+    Task = asyncio.current_task()
+    #report its deteils
+    print(Task)
+    
+#start the coroutine program
+asyncio.run(main())
